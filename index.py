adb15c63fb6bb7fe3ae423cf5b83b8989405e0a5
Jinle Chen <cjinle@gmail.com>
2014-01-14 19:49:05 +0800

    from git_push.sh

--- index.py
+++ index.py
@@ -6,15 +6,16 @@
 Lok 2014/1/10
 """
 
-
 from flask import Flask
+from flask import render_template
+from flask import url_for, redirect
 
 app = Flask(__name__)
 if not app.debug:
     import logging
     from logging import FileHandler
     from logging import Formatter
-    file_handler = FileHandler(filename='./log.log')
+    file_handler = FileHandler(filename='logs/index.log')
     file_handler.setFormatter(Formatter(
         '%(asctime)s %(levelname)s: %(message)s '
         '[in %(pathname)s:%(lineno)d]'
@@ -27,10 +28,15 @@ def link_list():
     return 'link list'
 
 @app.route('/hello')
-def hello():
-    app.logger.error("hello ............ ")
-    return 'hello world'
+@app.route('/hello/<name>')
+def hello(name=None):
+    css = url_for('static', filename='style.css')
+    return render_template('hello.html', name=name, css=css)
+
+@app.route('/about')
+def about():
+    return ''
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')  # in virtualbox
+    app.run(host='0.0.0.0', debug=True)  # in virtualbox
